b842ad2127873ead99b685bcc0ad65414f024e24
Kurt Cyrus Silvino <kurtsilv.dthy@gmail.com>
2025-09-06 17:57:22 +0800

    Create home screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,15 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Scaffold homepage = Scaffold(appBar: AppBar(title: Text('Homepage')));
+    Scaffold homepage = Scaffold(
+      appBar: AppBar(title: Text('Homepage')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // TODO: Implement button
+        },
+        child: const Icon(Icons.add)
+      ),
+    );
 
     return MaterialApp(
       home: homepage,
